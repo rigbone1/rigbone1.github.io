41b97579767e9eb1d033ba3bc73d28a3b562cde2
David Graham <d.graham8@rgu.ac.uk>
2022-02-20 20:34:11 +0000

    main: add configs and gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./dist/index.html'],
+  theme: {
+    fontFamily: {
+      body: ['Inter', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
